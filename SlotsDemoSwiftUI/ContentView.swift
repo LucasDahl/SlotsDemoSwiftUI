6b14746fee2e27742e38ce95fd45af105f7c4c0a
Lucas Dahl <LucasDahl425@yahoo.com>
2020-03-26 14:27:14 -0700

    Initial commit

---
+++ SlotsDemoSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlotsDemoSwiftUI
+//
+//  Created by Lucas Dahl on 3/26/20.
+//  Copyright © 2020 Lucas Dahl. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
