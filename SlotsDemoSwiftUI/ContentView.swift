dcc917635f150ac23a5842df2c8e176ae78ad71a
Lucas Dahl <LucasDahl425@yahoo.com>
2020-03-27 11:42:32 -0700

    Added the game logic.

--- SlotsDemoSwiftUI/ContentView.swift
+++ SlotsDemoSwiftUI/ContentView.swift
@@ -10,8 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     
+    // Properties
+    private var symbols = ["apple", "star", "cherry"]
+    private var betAmount = 5
+    
     // State properties
     @State private var credits = 1000
+    @State private var numbers = [0, 1, 2]
+    
     
     var body: some View {
         
@@ -62,19 +68,19 @@ struct ContentView: View {
                     
                     Spacer()
                     
-                    Image("apple")
+                    Image(symbols[numbers[0]])
                         .resizable()
                         .aspectRatio(1, contentMode: .fit)
                         .background(Color.white.opacity(0.5))
                         .cornerRadius(20)
                     
-                    Image("apple")
+                    Image(symbols[numbers[1]])
                         .resizable()
                         .aspectRatio(1, contentMode: .fit)
                         .background(Color.white.opacity(0.5))
                         .cornerRadius(20)
                     
-                    Image("apple")
+                    Image(symbols[numbers[2]])
                         .resizable()
                         .aspectRatio(1, contentMode: .fit)
                         .background(Color.white.opacity(0.5))
@@ -89,7 +95,19 @@ struct ContentView: View {
                 // Button
                 Button(action: {
                     
-                    self.credits += 1
+                    // Change the images
+                    self.numbers[0] = Int.random(in: 0...self.symbols.count - 1)
+                    self.numbers[1] = Int.random(in: 0...self.symbols.count - 1)
+                    self.numbers[2] = Int.random(in: 0...self.symbols.count - 1)
+                    
+                    // Check for a win
+                    if self.numbers[0] == self.numbers[1] && self.numbers[1] == self.numbers[2] {
+                        
+                        self.credits += self.betAmount * 10
+                        
+                    } else {
+                        self.credits -= self.betAmount
+                    }
                     
                 }) {
                     Text("Spin")
